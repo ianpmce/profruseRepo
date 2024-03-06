d3ec1032f9d9bdf78d8eed530979235be1b8dc8e
ianpmce <104048056+ianpmce@users.noreply.github.com>
2024-03-05 18:11:00 -0600

    Update loops1.py

--- loops1.py
+++ loops1.py
@@ -3,4 +3,5 @@
 for x in range(0, 3):
     for y in range(0, 12):
       print x, y
+#This file is printing x and y starting from 0 to 3 printing this out for x and 0 to 12 for printing out y 
       
